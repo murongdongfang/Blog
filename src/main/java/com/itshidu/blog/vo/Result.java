418fcba1bb382d524afd817eca80675ea7eff98f
xxh <xxh@qq.com>
2019-09-21 17:17:16 +0800

    基于springboot的商业博客系统，使用SSM构建111

---
+++ src/main/java/com/itshidu/blog/vo/Result.java
@@ -0,0 +1,36 @@
+package com.itshidu.blog.vo;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component
+public class Result extends HashMap<String, Object> {
+	
+	private Result() { } //禁止被new调用
+	
+	public static Result of(int code) {
+		Result er = new Result();
+		er.put("code", code);
+		return er;
+	}
+	public static Result of(int code,String msg) {
+		Result er = new Result();
+		er.put("code", code);
+		er.put("msg", msg);
+		return er;
+	}
+	public static Result of(int code,String msg,Object data) {
+		Result er = new Result();
+		er.put("code", code);
+		er.put("msg", msg);
+		er.put("data", data);
+		return er;
+	}
+	
+	public Result put(String key,Object value) {
+		super.put(key, value);
+		return this;
+	}
+	
+}
